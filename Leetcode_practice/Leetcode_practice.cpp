548b5a6d3a24d9f5b12b8c3fb2f501a8acce43de
cpprev <charles.duchamp@epita.fr>
2021-01-12 05:39:24 +0100

    setup

---
+++ Leetcode_practice/Leetcode_practice.cpp
@@ -0,0 +1,16 @@
+///
+/// Playground file
+///
+
+#include "lib/stdc++.h"
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    map<string, string> m;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
