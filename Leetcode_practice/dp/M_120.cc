a3d70189d1417fe119906eb8b0179132025f6c39
cpprev <charles.duchamp@epita.fr>
2021-01-12 05:40:06 +0100

    added problem 120 (dp, medium)

---
+++ Leetcode_practice/dp/M_120.cc
@@ -0,0 +1,41 @@
+/// <summary>
+/// DP approach
+/// https://leetcode.com/problems/triangle/
+/// </summary>
+class Solution {
+public:
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(triangle.size(), vector<int>());
+        for (int i = 0; i < triangle.size(); ++i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                dp[i].push_back(0);
+            }
+        }
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.size(); ++i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                // 1 parent
+                int parent = -1;
+                if (j != 0) {
+                    // 2 parents
+                    if (j < triangle[i - 1].size()) {
+                        parent = min(dp[i - 1][j], dp[i - 1][j - 1]);
+                    }
+                    else {
+                        parent = dp[i - 1][j - 1];
+                    }
+
+                }
+                else {
+                    parent = dp[i - 1][j];
+                }
+                dp[i][j] = triangle[i][j] + parent;
+            }
+        }
+
+        vector<int> vec = dp[triangle.size() - 1];
+        return *min_element(vec.begin(), vec.end());
+    }
+};
