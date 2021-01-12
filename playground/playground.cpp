5bf27046e2dc4d5d06a372ac1fa59e55fd38561e
cpprev <charles.duchamp@epita.fr>
2021-01-12 05:51:34 +0100

    change files

---
+++ playground/playground.cpp
@@ -0,0 +1,13 @@
+///
+/// playground file
+///
+
+#include "lib/stdc++.h"
+
+int main()
+{
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
