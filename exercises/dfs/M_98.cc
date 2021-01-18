31c55fbd967cf75388fa1bde87bb35f16a015baf
cpprev <charles.duchamp@epita.fr>
2021-01-18 21:07:10 +0100

    added exercise 98 (dfs, medium)

---
+++ exercises/dfs/M_98.cc
@@ -0,0 +1,23 @@
+/// <summary>
+/// dfs approach
+/// Time complexity : O(n), n being the number of nodes in the tree
+/// Space complexity : O(1)
+/// https://leetcode.com/problems/rotting-oranges/
+/// </summary>
+class Solution {
+public:
+    bool dfs(TreeNode* root, TreeNode* low, TreeNode* high)
+    {
+        if (not root)
+            return true;
+
+        if ((low and root->val <= low->val) or (high and root->val >= high->val))
+            return false;
+
+        return dfs(root->left, low, root) and dfs(root->right, root, high);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return dfs(root, nullptr, nullptr);
+    }
+};
