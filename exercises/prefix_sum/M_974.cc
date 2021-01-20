c7f13c87873ecf38c9a624406d0d3bf1a47acb32
cpprev <charles.duchamp@epita.fr>
2021-01-20 17:12:02 +0100

    added exercise 974 (prefix sum, medium)

--- exercises/prefix_sum/M_974.cc
+++ exercises/prefix_sum/M_974.cc
@@ -8,16 +8,13 @@ class Solution {
 public:
     int subarraysDivByK(vector<int>& A, int K) {
         int n = A.size(), sum = 0, res = 0;
-        unordered_map<int, int> F;
-        F[0] = 1;
+        unordered_map<int, int> F{ {0, 1} };
         for (int i = 0; i < n; ++i) {
             sum += A[i];
-            int key = sum % K;
-            if (key < 0)
-                key += K;
+            int key = sum % K < 0 ? sum % K + K : sum % K;
             if (F.count(key))
                 res += F[key];
-            F[key] += 1;
+            ++F[key];
         }
         return res;
     }
