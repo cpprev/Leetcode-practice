f46979ace426cbb5955b4efd64aea7a4b2a90757
cpprev <charles.duchamp@epita.fr>
2021-01-21 16:32:18 +0100

    added exercise 77 (backtracking, medium)

---
+++ exercises/backtracking/M_77.cc
@@ -0,0 +1,31 @@
+/// <summary>
+/// backtracking approach
+/// Time complexity : O(n*k)
+/// Space complexity : O(n*k)
+/// https://leetcode.com/problems/combinations/
+/// </summary>
+class Solution {
+public:
+    int N, K;
+
+    void backtrack(vector<vector<int>>& comb, vector<int>& cur, int curN) {
+        if (cur.size() == K) {
+            comb.push_back(cur);
+            return;
+        }
+
+        for (int i = curN; i <= N; ++i) {
+            cur.push_back(i);
+            backtrack(comb, cur, i + 1);
+            cur.pop_back();
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        N = n, K = k;
+        vector<vector<int>> comb;
+        vector<int> cur;
+        backtrack(comb, cur, 1);
+        return comb;
+    }
+};
