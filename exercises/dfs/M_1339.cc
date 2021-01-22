cacea318e125c59b7394083170b617dcc628747c
cpprev <charles.duchamp@epita.fr>
2021-01-22 05:22:12 +0100

    added exercise 1339 (dfs, medium)

--- exercises/dfs/M_1339.cc
+++ exercises/dfs/M_1339.cc
@@ -1,13 +1,13 @@
 /// <summary>
 /// dfs approach (2 passes in the tree)
-/// Time complexity : O(n), n being the number of nodes in the tree
-/// Space complexity : O(n)
+/// Time complexity : O(n), n being the number of nodes in the tree (beats 100%)
+/// Space complexity : O(n) (beats 100%)
 /// https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
 /// </summary>
 class Solution {
 public:
     int sum, MOD = int(1e9) + 7;
-    pair<long, long> max = { 0, 0 };
+    long max = 0;
 
     int getsum(TreeNode* root) {
         if (not root) return 0;
@@ -20,11 +20,8 @@ public:
         long left = dfs(root->left);
         long right = dfs(root->right);
 
-        long local = left * (sum - left);
-        if (local > max.first * max.second) max = { left, sum - left };
-
-        local = right * (sum - right);
-        if (local > max.first * max.second) max = { right, sum - right };
+        long local = std::max(left * (sum - left), right * (sum - right));
+        max = std::max(max, local);
 
         return root->val + left + right;
     }
@@ -34,6 +31,6 @@ public:
 
         dfs(root);
 
-        return (max.first * max.second) % MOD;
+        return max % MOD;
     }
 };
