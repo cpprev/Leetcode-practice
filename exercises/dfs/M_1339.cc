1bb922988f9a96c099422e69b0345fb6b9687486
cpprev <charles.duchamp@epita.fr>
2021-01-22 02:32:21 +0100

    added exercise 1339 (dfs, medium)

---
+++ exercises/dfs/M_1339.cc
@@ -0,0 +1,39 @@
+/// <summary>
+/// dfs approach
+/// Time complexity : O(n), n being the number of nodes in the tree
+/// Space complexity : O(n)
+/// https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+/// </summary>
+class Solution {
+public:
+    int sum, MOD = int(1e9) + 7;
+    pair<long, long> max = { 0, 0 };
+
+    int getSum(TreeNode* root) {
+        if (not root) return 0;
+        return root->val + getSum(root->left) + getSum(root->right);
+    }
+
+    int rec(TreeNode* root) {
+        if (not root) return 0;
+
+        long left = rec(root->left);
+        long right = rec(root->right);
+
+        long local = left * (sum - left);
+        if (local > max.first * max.second) max = { left, sum - left };
+
+        local = right * (sum - right);
+        if (local > max.first * max.second) max = { right, sum - right };
+
+        return root->val + left + right;
+    }
+
+    int maxProduct(TreeNode* root) {
+        sum = getSum(root);
+
+        rec(root);
+
+        return (max.first * max.second) % MOD;
+    }
+};
