48f0f24fd1697ab37361a51569abd215c3af0d2c
cpprev <charles.duchamp@epita.fr>
2021-02-07 13:28:16 +0100

    added exercise 62 (medium, dp)

--- exercises/dp/M_62.cc
+++ exercises/dp/M_62.cc
@@ -1,6 +1,6 @@
 /// <summary>
-/// DP approach : every element in the dp matrix is equal to the sum of the possible previous step
-/// ([i - 1][j] or [i][j - 1] since we can only go to the right or move down)
+/// DP approach : every element in the dp matrix is equal to the sum of the possible previous steps
+/// ([i - 1][j] and [i][j - 1] since we can only go to the right or move down)
 /// Time complexity : O(n*n)
 /// Space complexity : O(m*n)
 /// https://leetcode.com/problems/unique-paths/
