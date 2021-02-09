26ee75da353e5d33d95997562fa66d6ccc5aefc6
cpprev <charles.duchamp@epita.fr>
2021-02-09 15:03:46 +0100

    added exercise 954 (medium, hashmap)

---
+++ exercises/hashmap/M_954.cc
@@ -0,0 +1,30 @@
+/// <summary>
+/// Store occurences in hashmap approach
+/// Time complexity : O(nlogn) (because of sort)
+/// Space complexity : O(n)
+/// https://leetcode.com/problems/array-of-doubled-pairs/
+/// </summary>
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+        int n = arr.size(), res = 0;
+        sort(arr.begin(), arr.end());
+        
+        unordered_map<int, int> dp;
+        for (auto& e : arr) ++dp[e];
+
+        for (auto& elem : arr) {
+            auto it = dp.find(elem);
+            int occ = it->second;
+            if (occ == 0 or (elem == 0 and occ % 2 == 1)) continue;
+            
+            auto dit = dp.find(2 * elem);
+            if (dit != dp.end() and dit->second > 0) {
+                --it->second;
+                --dit->second;
+                ++res;
+            }
+        }
+        return res == n / 2;
+    }
+};
