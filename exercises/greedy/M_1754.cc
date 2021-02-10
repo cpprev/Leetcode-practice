44844ed20dbbc7b899ccbf5de3bdd56ea9653d9d
cpprev <charles.duchamp@epita.fr>
2021-02-10 16:56:50 +0100

    added exercise 1754 (medium, greedy)

--- exercises/greedy/M_1754.cc
+++ exercises/greedy/M_1754.cc
@@ -8,14 +8,13 @@ class Solution {
 public:
     string largestMerge(string word1, string word2) {
         string res;
-        while (true) {
+        while (not word1.empty() or not word2.empty()) {
             bool w1 = true;
-            if (not (word1.empty() and word2.empty())) {
+            if (not word1.empty() and not word2.empty()) {
                 if (word1[0] < word2[0]) w1 = false;
                 else if (word1.compare(word2) < 0) w1 = false;
             }
             else if (not (word2.empty())) w1 = false;
-            else break;
             
             if (w1) {
                 res += word1[0];
